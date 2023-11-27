4823d2f9ab82f0c10417a4c84ddaa2f159b86409
fa-sharp <farshadkt@gmail.com>
2023-11-27 01:00:08 -0500

    eslint stuff

--- packages/core/src/listeners/midi.ts
+++ packages/core/src/listeners/midi.ts
@@ -9,18 +9,21 @@ export const setupMIDIListener = (onAddNote: (midiNum: number) => void) => {
     if (ev.data[0] === 144 && ev.data[1]) onAddNote(ev.data[1]);
   };
 
-  navigator.requestMIDIAccess().then((midiAccess) => {
-    inputs = midiAccess.inputs;
-    inputs.forEach((input) => {
-      //@ts-ignore
-      input.addEventListener("midimessage", listener);
-    });
-  });
+  navigator
+    .requestMIDIAccess()
+    .then((midiAccess) => {
+      inputs = midiAccess.inputs;
+      inputs.forEach((input) => {
+        //@ts-expect-error FIXME wrong typing https://github.com/microsoft/TypeScript-DOM-lib-generator/pull/1652
+        input.addEventListener("midimessage", listener);
+      });
+    })
+    .catch((err) => console.error("Error getting MIDI access:", err));
 
   return () => {
     inputs?.forEach(
       (
-        input //@ts-ignore
+        input //@ts-expect-error  FIXME wrong typing https://github.com/microsoft/TypeScript-DOM-lib-generator/pull/1652
       ) => input.removeEventListener("midimessage", listener)
     );
     inputs = null;
