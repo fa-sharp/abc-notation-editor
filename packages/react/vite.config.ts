016f0b554faefbd6b77feb4f714ed7efacf2a135
fa-sharp <farshadkt@gmail.com>
2023-12-31 00:48:17 -0500

    don't auto-inject CSS

--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -3,7 +3,6 @@ import path from "node:path";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import dts from "vite-plugin-dts";
-import { libInjectCss } from "vite-plugin-lib-inject-css";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -39,6 +38,5 @@ export default defineConfig({
     react(),
     tsconfigPaths(),
     dts({ entryRoot: "./src", rollupTypes: true }),
-    libInjectCss(),
   ],
 });
