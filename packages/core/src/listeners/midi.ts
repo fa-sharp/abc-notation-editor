0ebae1c8eec9ef962f41b6330a244bff23b43a02
fa-sharp <farshadkt@gmail.com>
2024-04-14 22:12:16 -0400

    update midi

--- packages/core/src/listeners/midi.ts
+++ packages/core/src/listeners/midi.ts
@@ -1,30 +1,27 @@
 /**
  * Setup a MIDI event listener, to add notes via a MIDI keyboard. Listens for the
- * MIDI `note-on` event on all detected MIDI inputs. */
+ * MIDI `note-on` event on all detected MIDI inputs. Returns a cleanup function to remove the listeners. */
 export const setupMIDIListener = (onAddNote: (midiNum: number) => void) => {
   let inputs: MIDIInputMap | null = null;
 
   const listener = (ev: MIDIMessageEvent) => {
     // 144 is the `note-on` event code
-    if (ev.data[0] === 144 && ev.data[1]) onAddNote(ev.data[1]);
+    if (ev?.data?.[0] === 144 && ev.data[1]) onAddNote(ev.data[1]);
   };
 
   navigator
-    .requestMIDIAccess()
+    .requestMIDIAccess({ sysex: true })
     .then((midiAccess) => {
       inputs = midiAccess.inputs;
       inputs.forEach((input) => {
-        //@ts-expect-error FIXME wrong typing https://github.com/microsoft/TypeScript-DOM-lib-generator/pull/1652
         input.addEventListener("midimessage", listener);
       });
     })
     .catch((err) => console.error("Error getting MIDI access:", err));
 
   return () => {
-    inputs?.forEach(
-      (
-        input //@ts-expect-error  FIXME wrong typing https://github.com/microsoft/TypeScript-DOM-lib-generator/pull/1652
-      ) => input.removeEventListener("midimessage", listener)
+    inputs?.forEach((input) =>
+      input.removeEventListener("midimessage", listener)
     );
     inputs = null;
   };
