8eeb5733ac0fd94252bacce826bc2a23e4418c62
fa-sharp <farshadkt@gmail.com>
2024-04-26 00:47:47 -0400

    explicit exports (might work around bundler issues)

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,4 +1,13 @@
-export * from "./listeners";
-export * from "./state";
-export * from "./types";
-export * from "./icons";
+export {
+  setupKeyboardListener,
+  setupMIDIListener,
+  setupStaffMouseListeners,
+} from "./listeners";
+export {
+  type EditorCommandAction,
+  editorCommandReducer,
+  type EditorCommandState,
+  EditorState,
+} from "./state";
+export { Accidental, Rhythm } from "./types";
+export { Icon, getIcon } from "./icons";
