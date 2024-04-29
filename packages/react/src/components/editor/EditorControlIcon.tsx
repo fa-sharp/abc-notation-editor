31c0674d2cddb0b8d95a115079e50264cde529e2
fa-sharp <farshadkt@gmail.com>
2024-04-29 07:13:22 -0400

    react: render svg slightly differently

--- packages/react/src/components/editor/EditorControlIcon.tsx
+++ packages/react/src/components/editor/EditorControlIcon.tsx
@@ -10,18 +10,21 @@ const EditorControlIcon = ({ icon, size }: Props) => {
   const rawSvg = useMemo(() => getIcon(icon), [icon]);
 
   const refCallback = useCallback(
-    (ref: HTMLDivElement | null) => {
-      if (!ref || !rawSvg) return;
-      const svg = ref.querySelector("svg");
-      if (svg && size) {
+    (div: HTMLDivElement | null) => {
+      if (!div || !rawSvg) return;
+      const tempDiv = document.createElement("div");
+      tempDiv.innerHTML = rawSvg;
+      const svg = tempDiv.firstChild;
+      if (svg instanceof SVGElement && size) {
         svg.setAttribute("height", size.toString());
         svg.setAttribute("width", size.toString());
       }
+      svg && div.replaceChildren(svg);
     },
     [rawSvg, size]
   );
 
-  return <div ref={refCallback} dangerouslySetInnerHTML={{ __html: rawSvg }} />;
+  return <div ref={refCallback} />;
 };
 
 export default EditorControlIcon;
